31cf8c4f3892a2c4ce0c0e95adc763890620dc7f
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2023-04-11 12:44:19 +0300

    Simple express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} 🚀`);
+});
