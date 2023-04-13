4cecd92dc8ab37b0b8506b4a3e82001cc755afd1
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2023-04-13 09:35:44 +0300

    Add JWT expiration time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,12 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const secret = process.env.JWT_SECRET || 'secret'; // TODO: fix this
+const expiresIn = process.env.JWT_EXPIRES_IN || '1d';
 
 export function verifyJwt(token: string): string | JwtPayload {
-  return jwt.verify(token, secret!);
+  return jwt.verify(token, secret);
 }
 
 export function signJwt(payload: object): string {
-  return jwt.sign(payload, secret!);
+  return jwt.sign(payload, secret, { expiresIn });
 }
